242372aeca953d4be81c4deb83ff9cddd69189cb
sssv587 <1092663295@qq.com>
2023-01-11 00:26:00 +0800

    角色管理模块-整合Swagger2

--- service-system/src/main/java/com/futurebytedance/system/controller/SysRoleController.java
+++ service-system/src/main/java/com/futurebytedance/system/controller/SysRoleController.java
@@ -2,10 +2,10 @@ package com.futurebytedance.system.controller;
 
 import com.futurebytedance.model.system.SysRole;
 import com.futurebytedance.system.service.SysRoleService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,6 +15,7 @@ import java.util.List;
  * @date 2023/1/10 - 23:54
  * @Description
  */
+@Api(tags = "角色管理接口")
 @RestController
 @RequestMapping("/admin/system/sysRole")
 public class SysRoleController {
@@ -23,9 +24,18 @@ public class SysRoleController {
 
     //http://localhost:8800/admin/system/sysRole/findAll
     //1.查询所有记录
+    @ApiOperation("查询所有记录")
     @GetMapping("findAll")
     public List<SysRole> findAddRole() {
         //调用service
         return sysRoleService.list();
+    }
+
+    //2.逻辑删除接口
+    @ApiOperation("逻辑删除接口")
+    @DeleteMapping("remove/{id}")
+    public boolean removeRole(@PathVariable Long id) {
+        //调用方法删除
+        return sysRoleService.removeById(id);
     }
 }
