e0b49ce797f4b4710267aff29a58d051afb9dec9
sssv587 <1092663295@qq.com>
2023-02-01 00:33:24 +0800

    权限管理模块-JWT整合

---
+++ common/common-util/src/main/java/com/futurebytedance/common/utils/JwtHelper.java
@@ -0,0 +1,70 @@
+package com.futurebytedance.common.utils;
+
+import io.jsonwebtoken.*;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2023/2/1 - 0:22
+ * @Description 生成JSON Web令牌的工具类
+ */
+public class JwtHelper {
+    // token 过期时间
+    private static final long tokenExpiration = 365L * 24 * 60 * 60 * 1000;
+    // 加密秘钥
+    private static final String tokenSignKey = "123456";
+
+    //根据用户id和用户名称生成token字符串
+    public static String createToken(Long userId, String username) {
+        return Jwts.builder()
+                .setSubject("AUTH-USER")
+                .setExpiration(new Date(System.currentTimeMillis() + tokenExpiration))
+                // 有效载荷
+                .claim("userId", userId)
+                .claim("username", username)
+                .signWith(SignatureAlgorithm.HS512, tokenSignKey)
+                .compressWith(CompressionCodecs.GZIP)
+                .compact();
+    }
+
+    public static Long getUserId(String token) {
+        try {
+            if (StringUtils.isEmpty(token)) return null;
+
+            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(tokenSignKey).parseClaimsJws(token);
+            Claims claims = claimsJws.getBody();
+            Integer userId = (Integer) claims.get("userId");
+            return userId.longValue();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static String getUsername(String token) {
+        try {
+            if (StringUtils.isEmpty(token)) return "";
+
+            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(tokenSignKey).parseClaimsJws(token);
+            Claims claims = claimsJws.getBody();
+            return (String) claims.get("username");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void removeToken(String token) {
+        //jwt token无需删除，客户端扔掉即可。
+    }
+
+//    public static void main(String[] args) {
+//        String token = JwtHelper.createToken(1L, "admin");//"eyJhbGciOiJIUzUxMiIsInppcCI6IkdaSVAifQ.H4sIAAAAAAAAAKtWKi5NUrJSCjAK0A0Ndg1S0lFKrShQsjI0MzY2sDQ3MTbQUSotTi3yTFGyMjKEsP0Sc1OBWp6unfB0f7NSLQDxzD8_QwAAAA.2eCJdsJXOYaWFmPTJc8gl1YHTRl9DAeEJprKZn4IgJP9Fzo5fLddOQn1Iv2C25qMpwHQkPIGukTQtskWsNrnhQ";//JwtHelper.createToken(7L, "admin");
+//        System.out.println("token=>" + token);
+//        System.out.println(JwtHelper.getUserId(token));
+//        System.out.println(JwtHelper.getUsername(token));
+//    }
+}
